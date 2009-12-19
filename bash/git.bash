66edd98fae1039c8f9c3aea3ccffae015af99494
Yanick Champoux <yanick@babyl.dyndns.org>
2009-12-19 16:38:41 -0500

    no need to split for now

--- bash/git.bash
+++
@@ -1,7 +0,0 @@
-###########################
-# Git 
-###########################
-
-export GIT_EDITOR=vim
-source ~/.bash/git-completion.bash
-PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
--- bash/subversion.bash
+++
@@ -1,1 +0,0 @@
-export SVN_EDITOR=vim
