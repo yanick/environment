ee4e07cb0519ae4a9db2725974a321e9d2ebc8e0
Yanick Champoux <yanick@babyl.dyndns.org>
2011-03-05 19:16:19 -0500

    my_prompt

---
+++ bash/bits/my_prompt.bash
@@ -0,0 +1,46 @@
+txtblk='\033[0;30m' # Black - Regular
+txtred='\033[0;31m' # Red
+txtgrn='\033[0;32m' # Green
+txtylw='\033[0;33m' # Yellow
+txtblu='\033[0;34m' # Blue
+txtpur='\033[0;35m' # Purple
+txtcyn='\033[0;36m' # Cyan
+txtwht='\033[0;37m' # White
+bldblk='\033[1;30m' # Black - Bold
+bldred='\033[1;31m' # Red
+bldgrn='\033[1;32m' # Green
+bldylw='\033[1;33m' # Yellow
+bldblu='\033[1;34m' # Blue
+bldpur='\033[1;35m' # Purple
+bldcyn='\033[1;36m' # Cyan
+bldwht='\033[1;37m' # White
+unkblk='\033[4;30m' # Black - Underline
+undred='\033[4;31m' # Red
+undgrn='\033[4;32m' # Green
+undylw='\033[4;33m' # Yellow
+undblu='\033[4;34m' # Blue
+undpur='\033[4;35m' # Purple
+undcyn='\033[4;36m' # Cyan
+undwht='\033[4;37m' # White
+bakblk='\033[40m'   # Black - Background
+bakred='\033[41m'   # Red
+badgrn='\033[42m'   # Green
+bakylw='\033[43m'   # Yellow
+bakblu='\033[44m'   # Blue
+bakpur='\033[45m'   # Purple
+bakcyn='\033[46m'   # Cyan
+bakwht='\033[47m'   # White
+txtrst='\033[0m'    # Text Reset
+
+function shortname {
+    whoami | perl -pe's/yanick/me/'
+}
+
+function shorthost {
+    hostname | perl -pe's/enkidu/E/'
+}
+
+HOSTNAME=`shorthost`
+USER=`shortname`
+
+export PS1="\[$txtblu\]${USER}@${HOSTNAME}\[$txtblk\] \\w\[$txtgrn\]\$(__git_ps1)\[$txtblu\]\\\$\[$txtrst\] "
