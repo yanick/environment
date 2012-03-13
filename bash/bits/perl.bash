e3b3efde61b749f81de7bd663e69cfc019877c17
Yanick Champoux <yanick@babyl.dyndns.org>
2012-03-13 16:52:09 -0400

    tweakings

--- bash/bits/perl.bash
+++ bash/bits/perl.bash
@@ -1,3 +1,5 @@
 # parallelism ftw
 # http://www.modernperlbooks.com/mt/2012/01/speed-up-perlbrew-with-test-parallelism.html
-TEST_JOBS=9
+export TEST_JOBS=9
+
+export DBIC_TRACE_PROFILE=console
---
+++ bash/conf/perldoc-complete.bash
@@ -0,0 +1,2 @@
+alias pod=perldoc
+complete -C perldoc-complete -o nospace -o default pod
